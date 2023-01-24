507c4a7d11eb4b333c7c1249c2b473dbd7e4f0b1
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-01-24 08:51:53 +0000

    chore: update prisma seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 import seed from '../data.json' assert { type: 'json' };
 
-const prisma = new PrismaClient();
+(async () => {
+	const prisma = new PrismaClient();
 
-async function main() {
-	const data = seed.map(({ isBookmarked: _, ...rest }) => ({ ...rest }));
+	try {
+		console.log('running');
 
-	await prisma.item.deleteMany();
-	await prisma.item.createMany({ data });
-}
-main()
-	.then(async () => {
-		await prisma.$disconnect();
-	})
-	.catch(async (e) => {
-		console.error(e);
-		await prisma.$disconnect();
-		process.exit(1);
-	});
+		const data = seed.map(({ isBookmarked: _, ...rest }) => ({ ...rest }));
+
+		await prisma.item.deleteMany();
+		await prisma.item.createMany({ data });
+	} catch (error) {
+		console.error(error);
+	} finally {
+		prisma.$disconnect();
+	}
+})();
