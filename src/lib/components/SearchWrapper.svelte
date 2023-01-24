ee18d25b1ac572b4ee4ef43e7e5b3691531c2e45
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-01-24 19:06:51 +0000

    feat: add section, search wrapper component

---
+++ src/lib/components/SearchWrapper.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { Item } from '@prisma/client';
+	import Section from './Section.svelte';
+
+	export let searchData: Item[];
+	export let placeholder = 'Search for movies or TV series';
+
+	let search = '';
+
+	$: filteredItems = searchData.filter((item) => {
+		return search && item.title.toLowerCase().includes(search.toLowerCase());
+	});
+</script>
+
+<label class="my-8 flex h-6 gap-x-4 outline-none md:h-8 md:gap-x-7">
+	<img src="/assets/icon-search.svg" alt="Search" />
+	<input
+		bind:value={search}
+		type="text"
+		class="bg-blue-dark text-white focus:outline-none md:text-heading"
+		{placeholder}
+	/>
+</label>
+
+{#if search}
+	<Section items={filteredItems} title={`Found ${filteredItems.length} results for '${search}'`} />
+{:else}
+	<slot />
+{/if}
---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import type { Item } from '@prisma/client';
+	import { fade } from 'svelte/transition';
+	import Card from './Card.svelte';
+
+	export let items: Item[];
+	export let title: string;
+	export let type: Card['$$prop_def']['type'] = 'default';
+
+	$: trending = type === 'trending';
+</script>
+
+<section class="space-y-6">
+	<h1 class="text-heading-l">{title}</h1>
+
+	<ul class:trending class:default={!trending}>
+		{#each items as item, index (item.id)}
+			<li in:fade={{ delay: index * 100, duration: 100 }} out:fade={{ duration: 100 }}>
+				<Card {item} {type} />
+			</li>
+		{:else}
+			<li>
+				<p>There are no items</p>
+			</li>
+		{/each}
+	</ul>
+</section>
+
+<style lang="postcss">
+	.trending {
+		@apply flex gap-x-4 overflow-scroll;
+	}
+
+	.default {
+		@apply grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-x-[30px] md:gap-y-6 lg:grid-cols-4 lg:gap-x-10 lg:gap-y-8;
+	}
+</style>
