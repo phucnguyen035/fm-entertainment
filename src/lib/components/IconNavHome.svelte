ea72cb28128f160b0bce1f877ad45191189c9391
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-01-24 19:19:54 +0000

    fix: fill white not applied for icon nav

--- src/lib/components/IconNavHome.svelte
+++ src/lib/components/IconNavHome.svelte
@@ -6,7 +6,8 @@
 	><path
 		d="M8 0H1C.4 0 0 .4 0 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1Zm0 11H1c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1v-7c0-.6-.4-1-1-1ZM19 0h-7c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1Zm0 11h-7c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1v-7c0-.6-.4-1-1-1Z"
 		fill="#5A698F"
+		class="transition-colors group-hover:fill-white"
+		class:fill-blue-grey={!active}
 		class:fill-white={active}
-		class="fill-blue-grey transition-colors group-hover:fill-white"
 	/></svg
 >
