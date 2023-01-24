5e62b324c8594329d7fc15cabae8c46295c333f4
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-01-24 14:05:39 +0000

    feat: add common button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	export let type: HTMLButtonAttributes['type'] = 'button';
+	export let padding: 'normal' | 'small' = 'normal';
+	export let fullWidth = false;
+</script>
+
+<button
+	{type}
+	class:w-full={fullWidth}
+	class:py-2={padding === 'small'}
+	class="rounded-md bg-red p-3.5 text-center"
+	on:click
+>
+	<slot />
+</button>
