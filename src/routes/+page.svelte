3f042507dfb58df399f2a8afacd513c3d72679ea
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-01-24 19:07:33 +0000

    feat: complete index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,14 @@
 <script lang="ts">
-	import Card from '$lib/components/Card.svelte';
-	import { getImageThumbnailUrl } from '$lib/utils/getThumbnailUrl';
+	import SearchWrapper from '$lib/components/SearchWrapper.svelte';
+	import Section from '$lib/components/Section.svelte';
 	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
 </script>
 
-<div class="space-y-4">
-	<section>
-		<h1 class="text-heading-l">Trending</h1>
-
-		<ul>
-			{#each data.trending as item (item.id)}
-				<li>
-					<h3>{item.title}</h3>
-					<img src={getImageThumbnailUrl(item.thumbnail)} alt={item.title} />
-				</li>
-			{:else}
-				<li>
-					<p>There are no items</p>
-				</li>
-			{/each}
-		</ul>
-	</section>
-
-	<section>
-		<h1 class="text-heading-l">Recommended for you</h1>
-
-		<ul
-			class="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-x-[30px] md:gap-y-6 lg:grid-cols-4 lg:gap-x-10 lg:gap-y-8"
-		>
-			{#each data.items as item (item.id)}
-				<li>
-					<Card {item} />
-				</li>
-			{:else}
-				<li>
-					<p>There are no trending items</p>
-				</li>
-			{/each}
-		</ul>
-	</section>
-</div>
+<SearchWrapper searchData={data.items}>
+	<div class="space-y-4">
+		<Section items={data.trending} title="Trending" type="trending" />
+		<Section items={data.items} title="Recommended for you" />
+	</div>
+</SearchWrapper>
